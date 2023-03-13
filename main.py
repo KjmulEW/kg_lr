6f70ed9290ed4059c5b6576f0d2f22e7edff33d8
tuanture <andreyabalandinsmr@yandex.ru>
2023-03-13 22:54:18 +0400

    not important who is wolf, really important who is woooooolf

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import math
 import kg_algs
 
 class image_obj:
+
+
+    CENTER_X=500
+    CENTER_Y=500
+    CENTER_Z=500
+
+
     def __init__(self):
         self.verts = []
         self.poly = []
@@ -21,3 +28,36 @@ class image_obj:
                     if flag:
                         break
         return self
+
+    def draw_vert(self, filename, k):
+        global CENTER_X, CENTER_Y
+        for item in self.v_arr:
+            y = -int(item[0] * k) + CENTER_Y
+            x = -int(item[1] * k) + CENTER_X
+            self.image_matrix_v[x, y] = [255, 255, 255]
+        image = Image.fromarray(self.image_matrix_v, mode='RGB')
+        image.save(filename)
+
+    def read_poly(self, file):
+        for x in file:
+            xsplit = x.split()
+            if xsplit:
+                flag = False
+                if xsplit[0] == "f":
+                    self.f_arr.append(
+                        list(map(int, [xsplit[1].split('/')[0], xsplit[2].split('/')[0], xsplit[3].split('/')[0]])))
+                    flag = True
+                else:
+                    if flag:
+                        break
+        return self
+
+    def draw_poly(self, filename, k):
+        for item in self.f_arr:
+            for i in range(3):
+                br_alg(-(self.v_arr[item[i] - 1][1] * k + CENTER_X), -(self.v_arr[item[i] - 1][0] * k + CENTER_Y),
+                       -(self.v_arr[item[(i+1)%3] - 1][1] * k + CENTER_X), -(self.v_arr[item[(i+1)%3] - 1][0] * k + CENTER_Y),
+                       self.image_matrix_f)
+        image = Image.fromarray(self.image_matrix_f, mode='RGB')
+        image.save(filename)
+
