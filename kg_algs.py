c7e641da53e08881dc5dbd1a7a080f7ae2771fe5
tuanture <andreyabalandinsmr@yandex.ru>
2023-03-13 22:12:09 +0400

    init

---
+++ kg_algs.py
@@ -0,0 +1,35 @@
+import numpy as np
+from PIL import Image
+import numpy as np
+import math
+
+def br_alg(x0, y0, x1, y1, image_matrix):
+    steep = False
+    if abs(x0 - x1) < abs(y0 - y1):
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+        steep = True
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+    dx = int(x1 - x0)
+    dy = int(y1 - y0)
+    if dx == 0: dx = 0.0001
+    derror = abs(dy / dx)
+    error = 0
+    y = int(y0)
+    x = int(x0)
+    while x <= x1:
+        if steep:
+            image_matrix[y, x] = [255, 255, 255]
+        else:
+            image_matrix[x, y] = [255, 255, 255]
+        error += derror
+        if error > 0.5:
+            if y1 > y0:
+                y += 1
+            else:
+                y -= 1
+            error -= 1
+        x += 1
+
