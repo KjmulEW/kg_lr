34be6e7ffb4390963f8dec8a7236a9d833b04c19
KjmulEW <mateksemat@gmail.com>
2023-05-11 01:09:50 +0400

    ghugug

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import kg_algs
 CENTER_X = 500
 CENTER_Y = 500
 CENTER_Z = 500
+d = 1000
 
 
 class image_obj:
@@ -31,11 +32,16 @@ class image_obj:
                         break
         return self
 
+    def projective_transform(x, y, z, d):
+        xp = d * x / (z + d)
+        yp = d * y / (z + d)
+        return [xp, yp]
     def draw_vert(self, filename, k):
         global CENTER_X, CENTER_Y
         for item in self.verts:
-            y = -int(item[0] * k) + CENTER_Y
-            x = -int(item[1] * k) + CENTER_X
+            x, y = self.projective_transform(-item[1], -item[0], -item[2], d)
+            x = int(x * k) + CENTER_X
+            y = int(y * k) + CENTER_Y
             self.image_matrix_v[x, y] = [255, 255, 255]
         image = Image.fromarray(self.image_matrix_v, mode='RGB')
         image.save(filename)
@@ -48,20 +54,20 @@ class image_obj:
                 if xsplit[0] == "f":
                     self.poly.append(
                         list(map(int, [xsplit[1].split('/')[0], xsplit[2].split('/')[0], xsplit[3].split('/')[0]])))
-                    self.normals.append(kg_algs.triangle_normal(self.verts[self.poly[-1][0]-1],self.verts[self.poly[-1][1]-1], self.verts[self.poly[-1][2]-1] ))
                     flag = True
                 else:
                     if flag:
                         break
-        return self
 
     def draw_poly(self, filename, k):
         for item in self.poly:
             for i in range(3):
-                kg_algs.br_alg(-(self.verts[item[i] - 1][1] * k + CENTER_X),
-                               -(self.verts[item[i] - 1][0] * k + CENTER_Y),
-                               -(self.verts[item[(i + 1) % 3] - 1][1] * k + CENTER_X),
-                               -(self.verts[item[(i + 1) % 3] - 1][0] * k + CENTER_Y),
+                x1, y1 = self.projective_transform(-self.verts[item[i] - 1][1], -self.verts[item[i] - 1][0], -self.verts[item[i] - 1][2], d)
+                x2, y2 = self.projective_transform(-self.verts[item[(i + 1) % 3] - 1][1], -self.verts[item[(i + 1) % 3] - 1][0], -self.verts[item[(i + 1) % 3] - 1][2], d)
+                kg_algs.br_alg(x1 * k + CENTER_X,
+                               y1 * k + CENTER_Y,
+                               x2 * k + CENTER_X,
+                               y2 * k + CENTER_Y,
                                self.image_matrix_f)
         image = Image.fromarray(self.image_matrix_f, mode='RGB')
         image.save(filename)
@@ -73,21 +79,26 @@ class image_obj:
             j+=1
             if tcos < 0:
                 for i in range(3):
-                    kg_algs.br_alg(-(self.verts[item[i] - 1][1] * k + CENTER_X),
-                                   -(self.verts[item[i] - 1][0] * k + CENTER_Y),
-                                   -(self.verts[item[(i + 1) % 3] - 1][1] * k + CENTER_X),
-                                   -(self.verts[item[(i + 1) % 3] - 1][0] * k + CENTER_Y),
+                    x1, y1 = self.projective_transform(-self.verts[item[i] - 1][1], -self.verts[item[i] - 1][0], -self.verts[item[i] - 1][2], d)
+                    x2, y2 = self.projective_transform(-self.verts[item[(i + 1) % 3] - 1][1], -self.verts[item[(i + 1) % 3] - 1][0], -self.verts[item[(i + 1) % 3] - 1][2], d)
+                    kg_algs.br_alg(x1 * k + CENTER_X,
+                                   y1 * k + CENTER_Y,
+                                   x2 * k + CENTER_X,
+                                   y2 * k + CENTER_Y,
                                    self.image_matrix_f)
         image = Image.fromarray(self.image_matrix_f, mode='RGB')
         image.save(filename)
 
-    def draw_triangle(self, x0, y0, z0, x1, y1, z1, x2, y2, z2, k, tcos,):
-        x0 = x0 * k + CENTER_X
-        y0 = y0 * k + CENTER_Y
-        x1 = x1 * k + CENTER_X
-        y1 = y1 * k + CENTER_Y
-        x2 = x2 * k + CENTER_X
-        y2 = y2 * k + CENTER_Y
+    def draw_triangle(self, x0, y0, z0, x1, y1, z1, x2, y2, z2, k, tcos):
+        x0, y0 = self.projective_transform(x0, y0, z0, d)
+        x1, y1 = self.projective_transform(x1, y1, z1, d)
+        x2, y2 = self.projective_transform(x2, y2, z2, d)
+        x0 = int(x0 * k) + CENTER_X
+        y0 = int(y0 * k) + CENTER_Y
+        x1 = int(x1 * k) + CENTER_X
+        y1 = int(y1 * k) + CENTER_Y
+        x2 = int(x2 * k) + CENTER_X
+        y2 = int(y2 * k) + CENTER_Y
         xmin = int(min(x0, x1, x2))
         ymin = int(min(y0, y1, y2))
         xmax = int(max(x0, x1, x2))
@@ -100,44 +111,47 @@ class image_obj:
         shade_color = [255 * tcos, 255 * tcos, 255 * tcos]
         for x in range(xmin, xmax + 1):
             for y in range(ymin, ymax + 1):
-                bar_cord = kg_algs.bar_cord(x, y, x0, y0, x1, y1, x2, y2)
+                bar_cord = kg_algs.bar_cord(x - CENTER_X / k , y - CENTER_Y / k , x0 - CENTER_X / k , y0 - CENTER_Y / k , x1 - CENTER_X / k , y1 - CENTER_Y / k , x2 - CENTER_X / k , y2 - CENTER_Y / k )
                 if bar_cord[0] > 0 and bar_cord[1] > 0 and bar_cord[2] > 0:
                     z_poly = z0*bar_cord[0] + z1*bar_cord[1] + z2*bar_cord[2]
                     if z_poly < self.z_buff[x,y]:
-                        self.image_matrix_t[x, y] = shade_color
-                        self.z_buff[x, y] = z_poly
+                        self.image_matrix_t[x,y] = shade_color
+                        self.z_buff[x,y] = z_poly
 
-    def draw_triangles(self, filename, k):
-        j = 0
+    def draw_triangles(self,filename,k):
+        j=0
         for item in self.poly:
-            x0 = -self.verts[item[0] - 1][1]
-            y0 = -self.verts[item[0] - 1][0]
-            z0 = -self.verts[item[0] - 1][2]
-            x1 = -self.verts[item[1] - 1][1]
+            x0=-self.verts[item[0]-1][1]
+            y0=-self.verts[item[0]-1][0]
+            z0=-self.verts[item[0]-1][2]
+            x1=-self.verts[item[1]-1][1]
             y1 = -self.verts[item[1] - 1][0]
             z1 = -self.verts[item[1] - 1][2]
             x2 = -self.verts[item[2] - 1][1]
             y2 = -self.verts[item[2] - 1][0]
             z2 = -self.verts[item[2] - 1][2]
             tcos = kg_algs.triangle_cos(self.normals[j])
-            j+=1
-            if tcos>0:
+            j += 1
+            if tcos > 0:
                 self.draw_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, k, tcos)
                 self.save_triangle(filename)
 
-    def save_triangle(self, filename):
-        image = Image.fromarray(self.image_matrix_t, mode="RGB")
-        image.save(filename)
-
-model1 = image_obj()
-model2 = image_obj()
-
-model1.read_vert(open("model_1.obj", "r"))
-model2.read_vert(open("model_2.obj", "r"))
-
-model1.read_poly(open("model_1.obj", "r"))
-model2.read_poly(open("model_2.obj", "r"))
+        def save_triangle(self, filename):
+            image = Image.fromarray(self.image_matrix_t, mode="RGB")
+            image.save(filename)
 
 
-#model2.draw_triangles("triangle2_14.png", 1 / 3)
-model1.draw_triangles("triangle2_24.png", 5000)
+
+        model1 = image_obj()
+        model2 = image_obj()
+
+        model1.read_vert(open("model_1.obj", "r"))
+        model2.read_vert(open("model_2.obj", "r"))
+
+        model1.read_poly(open("model_1.obj", "r"))
+        model2.read_poly(open("model_2.obj", "r"))
+
+        d = 1000  # расстояние от точки до плоскости проекции
+
+        # model2.draw_triangles("triangle2_14.png", 1 / 3)
+        model1.draw_triangles("triangle2_24.png", 5000)
